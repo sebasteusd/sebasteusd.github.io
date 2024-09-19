2c542f5374a30bba73dad8293b196f29cdae7505
Diego Sebastian Valdivia Colque <sebasteusd@gmail.com>
2024-09-19 17:20:43 -0500

    Add files via upload

---
+++ AgroFuturo.js
@@ -0,0 +1,9 @@
+// JavaScript para el desplazamiento suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
