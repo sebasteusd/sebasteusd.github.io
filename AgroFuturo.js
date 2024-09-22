4493c4035d0f14bfe314d438ffe62003f659b402
Cuts01 <149018151+Cuts01@users.noreply.github.com>
2024-09-22 17:13:43 -0500

    Update AgroFuturo.js

--- AgroFuturo.js
+++ AgroFuturo.js
@@ -1,13 +1,1 @@
-// JavaScript para el desplazamiento suave
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-});
+
