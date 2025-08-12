9d54b6f1f97f34cabc63832c4962a4b28805fbc8
Diego Sebastian Valdivia Colque <sebasteusd@gmail.com>
2025-08-11 21:45:15 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("[Depuración] Página de presentación cargada correctamente.");
+  console.log("[Validación] Todos los módulos están activos y visibles.");
+});
